2b4493a99ddea1335230c18b6a8e38756e52a971
martingrgv <martin.geor04@gmail.com>
2025-01-28 00:41:25 +0200

    Created secret list view models

---
+++ src/Keepass/Keepass.Wpf/Models/SecretListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Keepass.Wpf.Models
+{
+    public class SecretListViewModel
+    {
+        public IEnumerable<SecretViewModel> Secrets { get; set; }
+    }
+}
---
+++ src/Keepass/Keepass.Wpf/Models/SecretViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Keepass.Wpf.Models
+{
+    public class SecretViewModel
+    {
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Url { get; set; } = default!;
+        public string Note { get; set; } = default!;
+    }
+}
